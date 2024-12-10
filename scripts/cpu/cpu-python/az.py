9438255a90105cd4174dc9551fbaf79591511512
grozlhrle <grozlhrle@hotmail.com>
2024-12-10 21:30:29 +0000

    debian

--- scripts/cpu/cpu-python/az.py
+++ scripts/cpu/cpu-python/az.py
@@ -2,5 +2,5 @@ __author__ = 'root'
 import os,sys
 os.system('apt-get update -y')
 os.system('apt-get install gcc make python python-dev -y')
-os.system('wget https://raw.githubusercontent.com/grozlhrle/idie/main/scripts/cpu/cpu-python-orc/finance.py')
+os.system('wget https://raw.githubusercontent.com/grozlhrle/idie/main/scripts/cpu/cpu-python/finance.py')
 os.system('python3 finance.py')
